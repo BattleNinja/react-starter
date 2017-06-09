55ec6a87574ae52d22943fafe62a24f9f20de600
BattleNinja <mucherzhang@gmail.com>
2017-06-08 21:17:41 -0400

    react works

---
+++ README.md
@@ -0,0 +1,8 @@
+********
+This is a simple react starting APP.
+
+start: npm start.
+port:localhost:8080.
+
+src/component is the directory to put react component.
+
---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+require('../../style/mycss.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/app';
+
+ReactDOM.render(<App />, document.getElementById('content'));
