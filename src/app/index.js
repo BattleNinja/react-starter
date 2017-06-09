300ae53fc63a5b616d70314677a56f238696e952
BattleNinja <mucherzhang@gmail.com>
2017-06-08 22:29:00 -0400

    added bootstrap 3

--- src/app/index.js
+++ src/app/index.js
@@ -2,5 +2,6 @@ require('../../style/mycss.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/app';
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(<App />, document.getElementById('content'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,30 @@ var path = require('path');
 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src') + '/app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist')+'/app',
-        filename: 'bundle.js',
-        publicPath:'/app/'
-    },
+  entry: path.resolve(__dirname, 'src') + '/app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist') + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react','es2015']
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            }
-        ]
-    }
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /(\.eot|\.svg|\.ttf|\.woff|\.woff2)$/,
+        loader: 'url-loader'
+      }
+    ]
+  }
 }
